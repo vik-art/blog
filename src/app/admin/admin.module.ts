15d65f2c68ba2c65ba2400ac054ad6c8bd4d35ee
Viktoriia <krjuchenkova@gmail.com>
2022-01-06 00:15:17 +0200

    base structure & router added

---
+++ src/app/admin/admin.module.ts
@@ -0,0 +1,56 @@
+import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
+import { RouterModule } from "@angular/router";
+import { LoginPageComponent } from "./login-page/login-page.component";
+import { AdminLayoutComponent } from './shared/components/admin-layout/admin-layout.component';
+import { DashboardPageComponent } from './dashboard-page/dashboard-page.component';
+import { CreatePageComponent } from './create-page/create-page.component';
+import { EditPageComponent } from './edit-page/edit-page.component';
+
+@NgModule({
+    imports: [
+        CommonModule,
+        RouterModule.forChild([
+            {
+                path: '', 
+                component: AdminLayoutComponent,
+                children: [
+                    {
+                        path: '',
+                        redirectTo: '/admin/login',
+                        pathMatch: 'full',
+                    },
+                    {
+                        path: 'login', 
+                        component: LoginPageComponent,
+                    },
+                    {
+                        path: 'dashboard',
+                        component: DashboardPageComponent,
+                    },
+                    {
+                        path: 'create',
+                        component: CreatePageComponent,
+                    },
+                    {
+                        path: 'post/:id/edit',
+                        component: EditPageComponent,
+                    }
+                ]
+            }
+        ])
+    ],
+    exports: [
+        RouterModule,
+    ],
+    declarations: [
+      AdminLayoutComponent,
+      DashboardPageComponent,
+      CreatePageComponent,
+      EditPageComponent
+    ]
+})
+
+export class AdminModule {
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainLayoutComponent } from './shared/components/main-layout/main-layout.component';
+import { HomepageComponent } from './components/homepage/homepage.component';
+import { PostPageComponent } from './components/post-page/post-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainLayoutComponent,
+    HomepageComponent,
+    PostPageComponent
   ],
   imports: [
     BrowserModule,
