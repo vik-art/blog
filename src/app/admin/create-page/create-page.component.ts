842fd444ced07fd978ca4402d90673d3266a0934
Viktoriia <krjuchenkova@gmail.com>
2022-01-11 14:20:46 +0200

    quill added

--- src/app/admin/create-page/create-page.component.ts
+++ src/app/admin/create-page/create-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Post } from 'src/app/shared/components/interfaces';
 
 @Component({
   selector: 'app-create-page',
@@ -6,10 +8,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create-page.component.scss']
 })
 export class CreatePageComponent implements OnInit {
+  form!: FormGroup;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.form = new FormGroup({
+      title: new FormControl("", [
+        Validators.required
+      ]),
+      text: new FormControl("", [
+        Validators.required
+      ]),
+      author: new FormControl("", [
+        Validators.required
+      ])
+    })
   }
-
+  submit() {
+    if (this.form.invalid) {
+      return;
+    }
+    const post: Post = {
+      title: this.form.value.title,
+      text: this.form.value.text,
+      author: this.form.value.autor,
+      date: new Date()
+    }
+     console.log(post)
+  }
 }
